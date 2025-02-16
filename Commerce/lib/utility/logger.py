412c2739d51287bc500d438d6d633f7f6c505901
Kristoffer Gjøsund <krigjo25@gmail.com>
2025-02-16 15:03:59 +0100

    Update logger.py

--- Commerce/lib/utility/logger.py
+++ Commerce/lib/utility/logger.py
@@ -1,9 +1,5 @@
-#   Python's logger module is used to log messages to the console and to a file.
-
-#   Copyright © 2023 Kristoffer Gjøsund
-#   Licensed under MIT License 
-
-
+#  Handling the application logging
+#   Based Basic Python logging concepts by  https://last9.io/blog/python-logging-best-practices/#basic-python-logging-concepts
 #   Importing required dependencies
 import logging as Log
 from typing import Optional, Union
@@ -17,9 +13,9 @@ class Logger(object):
 
         """
             *   Initialize the logger
+            *   Set the logging level to DEBUG
             *   Set the logging format
             *   Set the logging handler
-            *   Set the logging level to DEBUG
             *   param name: str - default: Class name
         """
         
@@ -29,7 +25,9 @@ class Logger(object):
         self.log = Log.getLogger(f"{self.name}")
         self.log.setLevel(Log.DEBUG)
         
-        self.handler = False
+        #   Initialize the Flags
+        self.file_handler = False
+        self.console_handler = False
 
     def SetupHandler(self, handler:Union[Log.FileHandler | Log.StreamHandler]):
         
@@ -48,21 +46,19 @@ class Logger(object):
         """
             *   Add a console handler to the logger
         """
+        
         #   Ensure that the Flag is not set to True
-        if not self.handler:
-
+        if not self.console_handler:
+            
+            #   Set the flag
+            self.console_handler = True
+            
             #   Initializing the handler
             handler = Log.StreamHandler()
             self.SetupHandler(handler)
 
             #   Send message to the console
             self.log.info(f"{self.name} has been initialized.")
-            
-            #   Set the flag
-            self.handler = True
-
-        else:
-            self.log.warning(f"{self.name} Console handler already added.")
 
     def FileHandler(self):
 
@@ -70,7 +66,7 @@ class Logger(object):
             *   Add a file handler to the logger
         """
         #   Ensure that the Flag is not set to True
-        if not self.handler:
+        if not self.file_handler:
 
             #   Initializing the handler
             handler = Log.FileHandler(f"{self.name}.log")
@@ -80,10 +76,10 @@ class Logger(object):
             self.log.info(f"{self.name} has been initialized.")
             
             #   Set the flag
-            self.handler = True
+            self.file_handler = True
 
         else:
-            self.log.warning(f"{self.name} Console handler already added.")
+            self.log.warning(f"{self.name} Console handler already initialized")
 
     def info(self, message):
         self.log.info(message)
@@ -91,7 +87,7 @@ class Logger(object):
     def error(self, message):
         self.log.error(message)
     
-    def warning(self, message):
+    def warn(self, message):
         self.log.warning(message)
     
     def debug(self, message):
